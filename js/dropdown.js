73c9bc9f3aeef8af5ff7b82ed5e3d5155e65fbf5
Robert Burek <robertburek@wp.pl>
2022-12-05 22:28:12 +0100

    Oferty  -  dropdown

---
+++ js/dropdown.js
@@ -0,0 +1,36 @@
+const dropdown = document.querySelector('.dropdown-option');
+const dropdownBtns = document.querySelectorAll('.dropdown-btn');
+console.log(dropdown);
+console.log(dropdownBtns);
+
+function opendropdownItems() {
+	if(this.nextElementSibling.classList.contains('dropdown-active')) {
+		this.nextElementSibling.classList.remove('dropdown-active');
+	} else {
+		closedropdownItem();
+		this.nextElementSibling.classList.toggle('dropdown-active');
+	};
+};
+
+const closedropdownItem = () => {
+	const allActiveItems = document.querySelectorAll('.dropdown-info');
+	allActiveItems.forEach(item => item.classList.remove('dropdown-active'));
+}
+
+const clickOutsidedropdown = e => {
+    if (e.target.classList.contains('dropdown-btn') ||
+		e.target.hasAttribute('dropdown')
+	) { 
+        // console.log(e.target);
+	    // console.log(document.querySelector('[dropdown]').attributes[1]);
+
+        return; }
+	if (!e.target.hasAttribute('logging')) { 
+		// console.log('click in label');
+		closedropdownItem();
+	}
+};
+
+dropdownBtns.forEach(btn => btn.addEventListener('click', opendropdownItems));
+
+window.addEventListener('click', clickOutsidedropdown);
