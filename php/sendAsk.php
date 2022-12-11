e0ae53e5e09b0ec0f0ff3f2a1e29d4785664eda2
RobertB <robertburek@wp.pl>
2022-12-11 22:42:45 +0100

    Doprecyzowanie informacji w mailu

--- php/sendAsk.php
+++ php/sendAsk.php
@@ -1,20 +1,26 @@
 <?php
 
+header('Location: ' . $_SERVER['HTTP_REFERER'] . '#form');
+
 $name = $_POST['nameSurname'];
 $mail = $_POST['mailAdress'];
 $content = $_POST['contentMail'];
+$surface = $_POST['totalSurface'];
+$apartments = $_POST['numberApartments'];
 
 
-function sendInfoMail($nameInfo, $mailInfo, $contentInfo)
+function sendInfoMail($nameInfo, $mailInfo, $contentInfo, $surfaceInfo, $apartmentsInfo)
 {
     header("content-type: application/json; charset=utf-8");
     $headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit";
-    $message_body = " " . $nameInfo . " wysłał/a zapytanie o ofertę.\n";
-    $message_body .= "O treści: " . $contentInfo . " \n";
+    $message_body = " " . $nameInfo . " wysłał/a zapytanie ofertowe o treści:\n";
+    $message_body .= "\"" . $contentInfo . "\"\n";
+    $message_body .= "Powierzchnia całkowita nieruchomości: " . $surfaceInfo . " m2. \n";
+    $message_body .= "Ilość lokali: " . $apartmentsInfo . " sztuk. \n";
     $message_body .= "Odpowiedź odeślij na adres mailowy: " . $mailInfo . "\n";
-    $message_body .= "Info ze strony http://robertburek.pl/HC24/";
-    mail("robertburek@wp.pl", "Zapytanie od " . $nameInfo, $message_body, $headers);
+    // $message_body .= "Info ze strony http://robertburek.pl/HC24/"."\n";
+    $message_body .= "Info ze strony ".$_SERVER['HTTP_REFERER'];
+    mail("homecare.24@wp.pl", "Zapytanie od " . $nameInfo, $message_body, $headers);
 }
 
-header('Location: '.$_SERVER['HTTP_REFERER'].'#form');
-sendInfoMail($name, $mail, $content);
+sendInfoMail($name, $mail, $content, $surface, $apartments);
