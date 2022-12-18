8af29e455e3e276821b1934fa3b5a7fa8d4335ab
RobertB <robertburek@wp.pl>
2022-12-18 17:08:17 +0100

    Reakcja na PRACA

---
+++ js/job.js
@@ -0,0 +1,6 @@
+var jobBtn = document.getElementById('jobBtn');
+var jobContainer = document.getElementById('job');
+
+jobBtn.addEventListener("click", () => {
+    jobContainer.style.display = 'block';
+});
